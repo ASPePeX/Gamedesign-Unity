2318fa2a42c6c132c02cfc078e4a6f34f19d0997
Alexander Scheurer <mail@aspepex.net>
2014-06-14 09:14:08 +0200

    Aligning stuff to the grind on startup

---
+++ Assets/Scripts/ResetToGrid.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetToGrid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    transform.position = Statics.TileToPos(Statics.PosToTile(transform.position.x, transform.position.y));
+	}
+}

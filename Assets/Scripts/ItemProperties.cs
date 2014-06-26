d8bbd6b1b40362a9d55750ac882956a1c240b90c
Alexander Scheurer <mail@aspepex.net>
2014-06-26 11:24:45 +0200

    Item Properties

---
+++ Assets/Scripts/ItemProperties.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemProperties : MonoBehaviour
+{
+    public bool canHeal;
+    public int healAmount;
+
+    public bool doesDesinfect;
+
+    public bool isWeapon;
+    public int damageAmount;
+    public int weaponRange;
+
+    public bool isSecondaryWeapon;
+    public int ammoAmount;
+
+    public bool canInfect;
+    public float infectionChance;
+
+    public bool isArmor;
+    public float damageReduction;
+}
